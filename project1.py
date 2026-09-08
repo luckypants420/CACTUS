d590f677d8c6e8dca1ae47b0d52813356a683499
Dhari <dhari2049@hotmail.com>
2026-09-08 17:07:45 +0100

    p3

---
+++ project1.py
@@ -0,0 +1,31 @@
+def add_person():
+    name = input("what is your name? ")
+    age = input("how old r u ? ")
+    email = input("what is your email? ")
+    person = {
+        "name": name,
+        "age": age,
+        "email": email,
+    }
+    return person
+
+
+while True:
+    command = input(
+        "would you like to add, delete, or search for a person?, (enter q to exit!)"
+    ).lower()
+    people = []
+    if command == "add":
+        person = add_person()
+        people.append(person)
+        print("person added!")
+    elif command == "delete":
+        pass
+    elif command == "search":
+        pass
+    elif command == "q":
+        break
+    else:
+        print("invalid command")
+
+print(people)
