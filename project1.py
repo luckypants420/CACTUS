fe08b821fb42af87f0f2de66556eb8cec112e5b5
Dhari <dhari2049@hotmail.com>
2026-09-11 08:02:49 +0100

    deleting a person from the list

--- project1.py
+++ project1.py
@@ -10,6 +10,31 @@ def add_person():
     return person
 
 
+def delete_contact(people):
+    print("contact list size: ", len(people))
+    if not people:
+        print("contact list is empty")
+        return
+
+    for i, person in enumerate(people):
+        print(i + 1, "-", person["name"], "|", person["age"], "|", person["email"])
+
+    while True:
+        number = input("enter a number to delete: ")
+        try:
+            number = int(number)
+            if number <= 0 or number > len(people):
+                print("invalid number, out of range!")
+            else:
+                break
+        except ValueError:
+            print("invalid number")
+    people.pop(number - 1)
+    print("person deleted")
+
+
+# mutable property which means changes to the list will happen everywhere
+
 people = []
 
 while True:
@@ -21,7 +46,7 @@ while True:
         people.append(person)
         print("person added!")
     elif command == "delete":
-        pass
+        delete_contact(people)
     elif command == "search":
         pass
     elif command == "q":
